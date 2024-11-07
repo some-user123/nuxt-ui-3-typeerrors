4196ccee8c11b54a82bbd52904eae7a0de82a9e7
some-user123 <74715856+some-user123@users.noreply.github.com>
2024-11-07 20:23:11 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -4,23 +4,13 @@ export default defineAppConfig({
             slots: {
                 base: 'font-normal'
             },
-            variants: {
-                size: {
-                    lg: 'px-4 py-2',
-                }
-            },
-            compoundVariants: [
-                {
-                    color: 'error',
-                    variant: 'link',
-                    class: 'text-lg'
-                }
-            ]
         },
         dropdownMenu: {
             variants: {
                 active: {
-                    false: 'data-highlighted:before:bg-red-500',
+                    false: {
+                        item: 'data-highlighted:before:bg-red-500'
+                    }
                 }
             }
         }
